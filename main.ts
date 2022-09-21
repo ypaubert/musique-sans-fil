2c077193e5c8ebf4a3563e1894864c9b6c344db1
ypaubert <114052247+ypaubert@users.noreply.github.com>
2022-09-21 16:10:57 +0200

    Mise à jour main.ts

--- main.ts
+++ main.ts
@@ -4,12 +4,12 @@ namespace EmetteurRadioMIDI {
     * Emet des notes MIDI par radio
       */
     //% block
-    export function Jouer_la_note_par_radio (canalmidi: number, notemidi: number) {
+    export function Jouer_par_radio_canal_note (canalmidi: number, notemidi: number) {
         radio.setGroup(1)
         radio.sendNumber(1000 * canalmidi + notemidi)
     }
     //% block
-    export function Arreter_la_note_par_radio (canalmidi: number, notemidi: number) {
+    export function Arreter_par_radio_canal_note (canalmidi: number, notemidi: number) {
         radio.setGroup(1)
         radio.sendNumber(1000 * canalmidi + notemidi)
     }
