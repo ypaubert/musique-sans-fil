54502133246d9f5364ecea59f2a2db34280f336c
ypaubert <114052247+ypaubert@users.noreply.github.com>
2022-09-21 15:51:59 +0200

    Mise à jour pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,15 @@
-function Jouer_la_note_par_radio (canalmidi: number, notemidi: number) {
-    radio.setGroup(1)
-    radio.sendNumber(1000 * canalmidi + notemidi)
-}
-function Arreter_la_note_par_radio (canalmidi: number, notemidi: number) {
-    radio.setGroup(1)
-    radio.sendNumber(1000 * canalmidi + notemidi)
+
+namespace fun {
+    /**
+    * Emet des notes MIDI par radio
+      */
+    //% block
+    export function Jouer_la_note_par_radio (canalmidi: number, notemidi: number) {
+        radio.setGroup(1)
+        radio.sendNumber(1000 * canalmidi + notemidi)
+    }
+    export function Arreter_la_note_par_radio (canalmidi: number, notemidi: number) {
+        radio.setGroup(1)
+        radio.sendNumber(1000 * canalmidi + notemidi)
+    }
 }
